da03d0aa26ae62b65e389c78819635616f2c7c59
Andrew Persad <apersad@deloitte.com>
2020-01-21 15:09:25 -0500

    Moved Landing page main code into main page. First Fold Component now handles props

--- src/components/index/index.jsx
+++ src/components/index/index.jsx
@@ -1,6 +1,8 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
 import { withRouter } from "react-router-dom";
+import "./index.scss";
+
 import Header from "../header/header";
 import FirstFold from "../first-fold/first-fold";
 
@@ -9,10 +11,21 @@ import FirstFold from "../first-fold/first-fold";
 class MainPage extends Component {
 	render() {
 		const { location } = this.props;
+		const url1 = "https://i.picsum.photos/id/248/3360/1768.jpg?blur=1";
+		const url2 = "https://i.picsum.photos/id/250/3360/1768.jpg?blur=1";
+		const url3 = "https://i.picsum.photos/id/254/3360/1768.jpg?blur=1";
+
 		return (
 			<>
 				<Header pathName={location} />
-				<FirstFold />
+
+				<main>
+					<FirstFold bkgdImage={url1} />
+
+					<FirstFold bkgdImage={url2} />
+
+					<FirstFold bkgdImage={url3} />
+				</main>
 			</>
 		);
 	}
