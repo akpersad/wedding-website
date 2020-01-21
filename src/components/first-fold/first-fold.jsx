286c6a56a247c83887304e7670f69747c7a630f0
Andrew Persad <akpersad@gmail.com>
2020-01-21 00:13:24 -0500

    Updated Code to push online. Added sudo parrallax

--- src/components/first-fold/first-fold.jsx
+++ src/components/first-fold/first-fold.jsx
@@ -1,8 +1,130 @@
 import React, { Component } from "react";
+import "./first-fold.scss";
 
 class FirstFold extends Component {
 	render() {
-		return <div className="test-noe">Hello World</div>;
+		return (
+			<>
+				<main>
+					<section className="cd-section cd-section--bg-fixed">
+						<div className="container max-width-sm">
+							<div className="text-component text-center">
+								<h1>Alternate Fixed &amp; Scroll Backgrounds</h1>
+								<p>The effect is visible only past 1024px.</p>
+								<p>
+									<a href="https://codyhouse.co/gem/alternate-fixed-scroll-backgrounds/">
+										Article &amp; Download
+									</a>
+								</p>
+							</div>
+						</div>
+					</section>
+
+					<section className="cd-section">
+						<div className="container max-width-sm">
+							<div className="text-component">
+								<p>
+									Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolore
+									incidunt suscipit similique, dolor corrupti cumque qui
+									consectetur autem laborum fuga quas ipsam doloribus sequi,
+									mollitia, repellendus sapiente repudiandae labore rerum amet
+									culpa inventore, modi non. Quo nisi veritatis vitae nam, labore
+									fugit. Inventore culpa iusto, officia exercitationem. Voluptates
+									quibusdam odit odio incidunt consequatur, consectetur aspernatur
+									optio vitae molestias, quas repellendus fugit ullam culpa,
+									eligendi et dignissimos voluptatibus illum? Molestias aliquam
+									nostrum quasi ipsa culpa, iusto explicabo ut error, consequuntur
+									enim temporibus, adipisci tempora voluptate, id consequatur
+									mollitia eveniet blanditiis. Illo quod repellendus alias? Cum
+									rem doloremque adipisci accusantium? Saepe, necessitatibus!
+								</p>
+							</div>
+						</div>
+					</section>
+
+					<section className="cd-section cd-section--bg-fixed">
+						<div className="container max-width-sm">
+							<div className="text-component text-center">
+								<h2>Lorem ipsum dolor sit amet.</h2>
+							</div>
+						</div>
+					</section>
+
+					<section className="cd-section">
+						<div className="container max-width-sm">
+							<div className="text-component">
+								<p>
+									Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolore
+									incidunt suscipit similique, dolor corrupti cumque qui
+									consectetur autem laborum fuga quas ipsam doloribus sequi,
+									mollitia, repellendus sapiente repudiandae labore rerum amet
+									culpa inventore, modi non. Quo nisi veritatis vitae nam, labore
+									fugit. Inventore culpa iusto, officia exercitationem. Voluptates
+									quibusdam odit odio incidunt consequatur, consectetur aspernatur
+									optio vitae molestias, quas repellendus fugit ullam culpa,
+									eligendi et dignissimos voluptatibus illum? Molestias aliquam
+									nostrum quasi ipsa culpa, iusto explicabo ut error, consequuntur
+									enim temporibus, adipisci tempora voluptate, id consequatur
+									mollitia eveniet blanditiis. Illo quod repellendus alias? Cum
+									rem doloremque adipisci accusantium? Saepe, necessitatibus!
+									Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolore
+									incidunt suscipit similique, dolor corrupti cumque qui
+									consectetur autem laborum fuga quas ipsam doloribus sequi,
+									mollitia, repellendus sapiente repudiandae labore rerum amet
+									culpa inventore, modi non. Quo nisi veritatis vitae nam, labore
+									fugit. Inventore culpa iusto, officia exercitationem. Voluptates
+									quibusdam odit odio incidunt consequatur, consectetur aspernatur
+									optio vitae molestias, quas repellendus fugit ullam culpa,
+									eligendi et dignissimos voluptatibus illum? Molestias aliquam
+									nostrum quasi ipsa culpa, iusto explicabo ut error, consequuntur
+									enim temporibus, adipisci tempora voluptate, id consequatur
+									mollitia eveniet blanditiis. Illo quod repellendus alias? Cum
+									rem doloremque adipisci accusantium? Saepe, necessitatibus!
+								</p>
+							</div>
+						</div>
+					</section>
+
+					<section className="cd-section cd-section--bg-fixed">
+						<div className="container max-width-sm">
+							<div className="text-component text-center">
+								<h2>Lorem ipsum dolor sit amet.</h2>
+							</div>
+						</div>
+					</section>
+
+					<section className="cd-section">
+						<div className="container max-width-sm">
+							<div className="text-component">
+								<p>
+									Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolore
+									incidunt suscipit similique, dolor corrupti cumque qui
+									consectetur autem laborum fuga quas ipsam doloribus sequi,
+									mollitia, repellendus sapiente repudiandae labore rerum amet
+									culpa inventore, modi non. Quo nisi veritatis vitae nam, labore
+									fugit. Inventore culpa iusto, officia exercitationem. Voluptates
+									quibusdam odit odio incidunt consequatur, consectetur aspernatur
+									optio vitae molestias, quas repellendus fugit ullam culpa,
+									eligendi et dignissimos voluptatibus illum? Molestias aliquam
+									nostrum quasi ipsa culpa, iusto explicabo ut error, consequuntur
+									enim temporibus, adipisci tempora voluptate, id consequatur
+									mollitia eveniet blanditiis. Illo quod repellendus alias? Cum
+									rem doloremque adipisci accusantium? Saepe, necessitatibus!
+								</p>
+							</div>
+						</div>
+					</section>
+
+					<div className="cd-section cd-section--bg-fixed">
+						<div className="container max-width-sm">
+							<div className="text-component text-center">
+								<h2>Lorem ipsum dolor sit amet.</h2>
+							</div>
+						</div>
+					</div>
+				</main>
+			</>
+		);
 	}
 }
 
