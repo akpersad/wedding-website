e760298b31c9b04813d9d37e272e6c116994335e
Andrew Persad <akpersad@gmail.com>
2020-01-23 23:05:09 -0500

    Added Landing View Component

---
+++ src/components/landing-view/landing-view.jsx
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+
+import "../../styles/main.scss";
+
+class LandingView extends Component {
+	render() {
+		return (
+			<>
+				<div className="h-100 row align-items-center">
+					<div className="col">TEXT</div>
+				</div>
+			</>
+		);
+	}
+}
+
+export default LandingView;
