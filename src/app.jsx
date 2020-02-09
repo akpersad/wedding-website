e8683dab147daa8df3fee59ace06e179ee328e92
Andrew Persad <akpersad@gmail.com>
2020-02-09 00:09:24 -0500

    Added registry stuff

--- src/app.jsx
+++ src/app.jsx
@@ -7,6 +7,7 @@ import { BrowserRouter as Router, Route, Switch, Redirect } from "react-router-d
 import MainPage from "./components/index/index";
 import UsersPage from "./components/users/users";
 import Wedding from "./components/wedding/wedding";
+import Registry from "./components/registry/registry";
 import NotFoundPage from "./components/404/page404";
 
 class App extends Component {
@@ -17,7 +18,7 @@ class App extends Component {
 					<Route exact path="/" component={MainPage} />
 					<Route exact path="/wedding" component={Wedding} />
 					<Route exact path="/accomodations" component={UsersPage} />
-					<Route exact path="/registry" component={UsersPage} />
+					<Route exact path="/registry" component={Registry} />
 					<Route exact path="/rsvp" component={UsersPage} />
 					<Route exact path="/404" component={NotFoundPage} />
 					<Redirect to="/404" />
